9e122c48b71d11d0bd1a8849269cd91960307600
Aidan Haley <AidanHaley@utilita.co.uk>
2021-11-10 02:52:35 +0000

    Initial Commit

---
+++ CatGif.ly/CatGif_lyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatGif_lyApp.swift
+//  CatGif.ly
+//
+//  Created by Aidan Haley on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CatGif_lyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
